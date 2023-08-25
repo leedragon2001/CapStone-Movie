f7fb53e82c4b6429dd95717324235435fb2d6a76
LeeHuy <chunggiahuy041@gmail.com>
2023-08-26 00:00:36 +0700

    upload file

--- src/Services/configURL.js
+++ src/Services/configURL.js
@@ -7,10 +7,11 @@ import { store } from "../index";
 import { localStorageService } from "./localStorageService";
 
 // export const TOKEN = process.env.REACT_APP_API_TOKEN;
-export const TOKEN ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAzMSIsIkhldEhhblN0cmluZyI6IjIzLzAyLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY3NzExMDQwMDAwMCIsIm5iZiI6MTY0ODQwMDQwMCwiZXhwIjoxNjc3MjU4MDAwfQ.0byoDjBIIS6877xg7NwEnO16v5HOltI9AatD9OLB0Ys";;
+export const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MCIsIkhldEhhblN0cmluZyI6IjE4LzAxLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwNTUzNjAwMDAwMCIsIm5iZiI6MTY3NzQzMDgwMCwiZXhwIjoxNzA1NjgzNjAwfQ.s4X0R0Wi80X0f9MLJ2XYxRKJdQJBW27dwvkpfN03100";
 export const https = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL,
-    baseURL: "https://movienew.cybersoft.edu.vn",
+  //   baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://movienew.cybersoft.edu.vn",
   headers: {
     TokenCybersoft: TOKEN,
     Authorization: "Bearer " + localStorageService.user.get()?.accessToken,
